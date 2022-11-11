74797e1312dea04c9cfac4c2de93919fe653488f
dimakovalev25 <d.nozdryn@gmail.com>
2022-11-11 15:39:44 +0300

    use setState

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,13 +1,33 @@
 import './app-info.css';
+import {Component} from "react";
 
-const AppInfo = () => {
-    return (
-        <div className='app-info'>
-            <h1>employees records:</h1>
-            <h2>headcount:</h2>
-            <h2>give out a bonus:</h2>
-        </div>
-    )
-};
+class AppInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            task: 1
+        }
+    }
+
+    addTask = () => {
+        this.setState({
+            task: this.state.task +1
+        })
+    }
+
+    render() {
+        return (
+            <div className='app-info'>
+                <h1>employees records:</h1>
+                <h2>headcount:</h2>
+                <h2>give out a bonus:</h2>
+                <h2>task: {this.state.task}</h2>
+                <button
+                    onClick={this.addTask}
+                    className="btn btn-outline-light">add task</button>
+            </div>
+        )
+    }
+}
 
 export default AppInfo;
